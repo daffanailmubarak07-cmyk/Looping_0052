8fe89f1c2fd6311fa62759ea90bc40e24f30dc52
daffanailmubarak07-cmyk <daffanailmubarak07@gmail.com>
2026-03-31 19:38:00 +0700

    Refactor Looping.cpp to include while and do-while

--- Looping.cpp
+++ Looping.cpp
@@ -11,4 +11,32 @@ int main(){
     for(int i=0;i<=4;i++){
         cout<<"Teknologi Informasi UMY"<<endl;
     }
-    
+    cout<<endl;
+
+    //Untuk mendapatkan urutan angka acak yg berbeda
+    srand(time(0));
+
+    //Perulangan dengan WHILE
+    cout<<"PERULANGAN WHILE"<<endl;
+    x = 1 + rand()%10;
+
+    while(x<=5){
+        cout<<"Bilangan acak = "<<x<<endl;
+        x = rand()%10;
+    }
+
+    cout<<"Bilangan acak while yang terakhir = "<<x<<endl;
+    cout<<endl;
+
+    //Perulangan dengan DO...WHILE
+    cout<<"PERULANGAN DO...WHILE"<<endl;
+    x = 1 + rand()%10;
+
+    do{
+        cout<<"Bilangan acak = "<<x<<endl;
+        x = rand()%10;
+    }while(x<=5);
+
+    cout<<"Bilangan acak do-while yang terakhir = "<<x<<endl;
+    cout<<endl;
+}
